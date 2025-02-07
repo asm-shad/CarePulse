e9b70ddef62b5662965eb1d338733a0535e78409
Asm Shad (INT) <asm.shad@liz.com>
2025-02-07 11:02:22 +0600

    Home Page designed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
+import PatientForm from "@/components/forms/PatientForm";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Home</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+        </div>
+        <PatientForm></PatientForm>
+        <div className="text-14-regular mt-20 flex justify-between">
+          <p className="justify-items-end text-dark-600 xl:text-left">
+            © {new Date().getFullYear()} CarePluse
+          </p>
+
+          <Link href="/?admin=true" className="text-green-500">
+            Admin
+          </Link>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
